92c1750d9a07ad9aaf0e5c5189e7c6282f8e3e56
Ankush Dubey <ankushdubey099@gmail.com>
2022-08-22 17:41:42 +0530

    AllOospConcept

---
+++ Controllers/ApiOopsController.cs
@@ -0,0 +1,238 @@
+﻿using AllOopsConcept.Model;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AllOopsConcept.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ApiOopsController : ControllerBase
+    {
+        //Inheritance
+        [HttpGet, Route("forstudent1")]
+
+        // Single
+        public string StudentDetails1()
+        {
+
+            Student1 student1 = new();
+            student1.ID = 1;
+            student1.Name = "Shiv";
+            student1.Email = "shiv@gmail.com";
+            student1.Marks = 300;
+
+            return ("\n Id :" + student1.ID +
+                "\n Name :" + student1.Name +
+                "\n Email :" + student1.Email +
+                "\n Marks :" + student1.Marks);
+
+        }
+        [HttpGet, Route("forstudent2")]
+
+        // Multi
+        public string StudentDetails2()
+        {
+            Student2 student2 = new();
+            student2.ID = 2;
+            student2.Name = "Shiv";
+            student2.Email = "shiv@gmail.com";
+            student2.Marks = 300;
+            student2.CollegeName = "DU";
+
+            return ("\n Id :" + student2.ID +
+                "\n Name :" + student2.Name +
+                "\n Email :" + student2.Email +
+                "\n Marks :" + student2.Marks +
+                "\n College Name :" + student2.CollegeName);
+
+        }
+        [HttpGet, Route("forstudent3")]
+
+        // Hierarchical
+        public string StudentDetails3()
+        {
+            Student3 student3 = new();
+            student3.ID = 3;
+            student3.Name = "Shiv";
+            student3.Email = "shiv@gmail.com";
+            student3.Subject = "Computer Science";
+
+            return ("\n Id :" + student3.ID +
+                "\n Name :" + student3.Name +
+                "\n Email :" + student3.Email +
+                "\n Subject :" + student3.Subject);
+
+        }
+
+        [HttpGet, Route("forallstudent")]
+        public string StudentDetails()
+        {
+            Student1 student1 = new();
+            student1.ID = 1;
+            student1.Name = "Shiv";
+            student1.Email = "shiv@gmail.com";
+            student1.Marks = 300;
+
+
+            Student2 student2 = new();
+            student2.ID = 2;
+            student2.Name = "Shiv";
+            student2.Email = "shiv@gmail.com";
+            student2.Marks = 300;
+            student2.CollegeName = "DU";
+
+
+            Student3 student3 = new();
+            student3.ID = 3;
+            student3.Name = "Shiv";
+            student3.Email = "shiv@gmail.com";
+            student3.Subject = "Computer Science";
+
+            return ("Id :" + student1.ID +
+                "\n Name :" + student1.Name +
+                "\n Email :" + student1.Email +
+                "\n Marks :" + student1.Marks +
+                "\n Id :" + student2.ID +
+                "\n Name :" + student2.Name +
+                "\n Email :" + student2.Email +
+                "\n Marks :" + student2.Marks +
+                "\n College Name :" + student2.CollegeName +
+                "\n Id :" + student3.ID +
+                "\n Name :" + student3.Name +
+                "\n Email :" + student3.Email +
+                "\n Subject :" + student3.Subject);
+        }
+
+        // Class and Object 
+        [HttpGet, Route("forclassobject")]
+        public string ClassObject()
+        {
+            ClassObject classObject = new();
+            classObject.Employee(123, "Sunder", "sunder@gmail.com", "xyz", "9635264512");
+            return classObject.GetEmployee();
+        }
+
+
+        // Constructor
+
+        [HttpGet, Route("forconstructor")]
+        public string Constructor()
+        {
+            // Default
+            ConstructorClass constructorClass = new();
+
+            // Parameterized
+            ConstructorClass constructorClass1 = new ConstructorClass("Priti", 502, "AP", "Kashi Mira (Mumbai, Maharashtra)", "7896532145");
+
+            // Copy
+            ConstructorClass constructorClass2 = new ConstructorClass(constructorClass1);
+
+            // Static 
+
+            return (/*Default*/"Default" + "\n Name :" + constructorClass.Name +
+                "\n Id :" + constructorClass.Id +
+                "\n College Name :" + constructorClass.CollegeName +
+                "\n Adderess :" + constructorClass.Adderess +
+                "\n Phone number :" + constructorClass.PhoneNmber +
+                /* Parameterized*/ "\nParameterized " + "\n" + constructorClass1.GetDetails()
+                 +/* Copy*/ "\nCopy " + "\n" + constructorClass2.GetDetails() +
+                 /* Static*/ "\nStatic " + "\n" + ConstructorClass.GetDetailsStatic());
+        }
+
+        //Polymorphism
+
+        [HttpGet, Route("formethodoverloading")]
+
+        //Method Overloading
+        public string StudentDetailss()
+        {
+            UserInformationClass1 userInformationClass = new();
+            return (userInformationClass.Student1("Joun", "Maria") +
+                " \n Second Data  :" + userInformationClass.Student1("Joun", "Maria", 12));
+        }
+
+        //Operator Overloading
+
+        [HttpGet, Route("foroperatoroverloading")]
+        public string OperatorOverloading()
+        {
+            OperatorOverloading operatorOverloading1 = new();
+            operatorOverloading1.name = "Joun";
+            operatorOverloading1.number = 200;
+
+            OperatorOverloading operatorOverloading2 = new();
+            operatorOverloading2.name = "Maria";
+            operatorOverloading2.number = 200;
+
+            OperatorOverloading? operatorOverloading3 = new();
+            operatorOverloading3 = operatorOverloading1 + operatorOverloading2;
+            return operatorOverloading3?.name + " \n" + operatorOverloading3?.number;
+        }
+
+        // Overring
+
+        [HttpGet, Route("formethodoverrde")]
+        //Method Overloading
+        public string OverrideClass()
+        {
+            OverrdingClass1 overrdingClass1 = new();
+            OverrdingClass2 overrdingClass2 = new();
+
+            // Reference of child class
+            // OverrdingClass1 overrdingClass3 = new OverrdingClass2();
+
+            return overrdingClass1.Employee1() +
+                " \n" + overrdingClass2.Employee1()
+                /*+ " \n" + overrdingClass3.Employee1()*/;
+        }
+
+        // Abstract
+
+        [HttpGet, Route("forabstract")]
+
+        public string EmployeeDetails()
+        {
+
+            AbstractClass1 abstractClass1 = new AbstractClass1();
+            abstractClass1.Name = "Vikash";
+            abstractClass1.Id = 101;
+            abstractClass1.Email = "viskash@gmail.com";
+            abstractClass1.CompanyName = "OSP";
+            abstractClass1.PhoneNumber = "7896589652";
+            abstractClass1.Adderss = "Kandivali";
+
+            AbstractClass2 abstractClass2 = new AbstractClass2();
+            abstractClass2.Name = "Hemant";
+            abstractClass2.Id = 101;
+            abstractClass2.Email = "Hemant@gmail.com";
+            abstractClass2.CompanyName = "OSP";
+            abstractClass2.PhoneNumber = "7896589652";
+            abstractClass2.Adderss = "Kandivali";
+            abstractClass2.Salary = 789356.00f;
+
+            return (/*Abstract Class 1*/"Employee1" + "\n Name :" + abstractClass1.Name +
+          "\n Id :" + abstractClass1.Id +
+          "\n Email :" + abstractClass1.Email +
+          "\n Companay Name :" + abstractClass1.CompanyName +
+          "\n Phone Number :" + abstractClass1.PhoneNumber +
+          "\n Adderss :" + abstractClass1.Adderss +
+          /*Abstract Class 2*/"\nEmployee2" + "\n Name :" + abstractClass2.Name +
+          "\n Id :" + abstractClass2.Id +
+          "\n Email :" + abstractClass2.Email +
+          "\n Companay Name :" + abstractClass2.CompanyName +
+          "\n Phone Number :" + abstractClass2.PhoneNumber +
+          "\n Adderss :" + abstractClass2.Adderss);
+
+        }
+
+        //Interface 
+        [HttpGet, Route("forinterface")]
+        public string Get()
+        {
+            Student student = new();
+            student.Id = 2;
+            student.rollNo = 123;
+            return student.Show() + "\n " + "\n" + ((IStudent1)student).Show() + student.Id+"\n"+student.rollNo;
+        }
+    }
+}
---
+++ Model/UserInformationClass.cs
@@ -0,0 +1,271 @@
+﻿namespace AllOopsConcept.Model
+{
+    public class UserInformationClass
+    {
+        public string? Name { get; set; }
+        public string? Email { get; set; }
+
+        public int ID { get; set; }
+    }
+
+    public class Student1 : UserInformationClass
+    {
+        public int Marks { get; set; }
+    }
+    public class Student2 : Student1
+    {
+        public string? CollegeName { get; set; }
+
+    }
+    public class Student3 : UserInformationClass
+    {
+        public string? Subject { get; set; }
+
+    }
+
+    // Class and object
+    public class ClassObject
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public string? Email { get; set; }
+        public string? CompanyName { get; set; }
+        public string? PhoneNumber { get; set; }
+        public void Employee(int Id, string Name, string Email, string CompanyName, string PhoneNumber)
+        {
+            this.Id = Id;
+            this.Name = Name;
+            this.Email = Email;
+            this.CompanyName = CompanyName;
+            this.PhoneNumber = PhoneNumber;
+
+        }
+        public string GetEmployee()
+        {
+            return ("Id :" + Id + "\n Name :" + Name + "\n Email :" + Email + "\n Company Name :" + CompanyName + "\n PhoneNumber :" + PhoneNumber);
+        }
+    }
+
+    //Constructor Concept
+
+    public class ConstructorClass
+    {
+        // Static Variable
+        public static String Name_ { get; set; }
+        public static int Id_ { get; set; }
+        public static string College_Name { get; set; }
+        public static string Adderess_ { get; set; }
+        public static string PhoneNmber_ { get; set; }
+
+        // Instance Variable
+        public String Name { get; set; }
+        public int Id { get; set; }
+        public string CollegeName { get; set; }
+        public string Adderess { get; set; }
+        public string PhoneNmber { get; set; }
+
+        // Default
+        public ConstructorClass()
+        {
+            Name = "Joun";
+            Id = 501;
+            CollegeName = "SMS";
+            Adderess = "Uttar Pradesh";
+            PhoneNmber = "7896542315";
+        }
+        // Parameterized
+        public ConstructorClass(string name, int id, string collegeName, string adderess, string phoneNumber)
+        {
+            this.Name = name;
+            this.Id = id;
+            this.Id = id;
+            this.CollegeName = collegeName;
+            this.Adderess = adderess;
+            this.PhoneNmber = phoneNumber;
+        }
+        public string GetDetails()
+        {
+            return (" Name :" + Name + "\n Id :" + Id + "\n College Name :" +
+                           CollegeName + "\n Adderess :" + Adderess + "\n Phone number :" + PhoneNmber);
+        }
+
+        // Copy 
+        public ConstructorClass(ConstructorClass constructorClass2)
+        {
+            this.Name = constructorClass2.Name;
+            this.Id = constructorClass2.Id;
+            this.CollegeName = constructorClass2.CollegeName;
+            this.Adderess = constructorClass2.Adderess;
+            this.PhoneNmber = constructorClass2.PhoneNmber;
+        }
+
+        //Static
+        static ConstructorClass()
+        {
+            Name_ = "Maria";
+            Id_ = 503;
+            College_Name = "SMS";
+            Adderess_ = "Uttar Pradesh";
+            PhoneNmber_ = "7896542315";
+        }
+        public static string GetDetailsStatic()
+        {
+            return (" Name :" + Name_ + "\n Id :" + Id_ + "\n College Name :" +
+                            College_Name + "\n Adderess :" + Adderess_ + "\n Phone number :" + PhoneNmber_);
+        }
+
+    }
+
+
+    //Polymorphism
+
+
+    //Method Overloading
+    public class UserInformationClass1
+    {
+        public string Student1(string firstName, string lastName)
+        {
+
+            return (firstName + " " + lastName);
+        }
+        public string Student1(string firstName, string lastName, int id)
+        {
+            return (firstName + " " + lastName + "\n Id :" + id);
+        }
+    }
+    // Operator Overloading
+
+    public class OperatorOverloading
+    {
+        public string? name;
+
+        public int number;
+        public static OperatorOverloading? operator +(OperatorOverloading operatorOverloading1, OperatorOverloading operatorOverloading2)
+        {
+            OperatorOverloading operatorOverloading3 = new();
+            operatorOverloading3.name = operatorOverloading1.name + " " + operatorOverloading2.name;
+            operatorOverloading3.number = operatorOverloading1.number + operatorOverloading2.number;
+            return operatorOverloading3;
+        }
+
+    }
+
+
+    //  Overrding
+
+    public class OverrdingClass1
+    {
+        public virtual string Employee1()
+        {
+            return "Employee 1";
+        }
+
+    }
+    public class OverrdingClass2 : OverrdingClass1
+    {
+        public override string Employee1()
+        {
+            return "Employee 2";
+            // return base.Employee1() +"\n"+"Employee 2";
+        }
+    }
+
+    // Abstract
+
+    public abstract class AbstractClass
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public string? Email { get; set; }
+        public string? CompanyName { get; set; }
+        public string? PhoneNumber { get; set; }
+        public abstract string Employee();
+
+    }
+    public class AbstractClass1 : AbstractClass
+    {
+        public string? Adderss { get; set; }
+        public override string Employee()
+        {
+            return (" Name :" + Name + "\n Id :" + Id + "\n Company Name :" + CompanyName + "\n Phone Number :" + PhoneNumber + "\n Adderss :" + Adderss);
+        }
+    }
+    public class AbstractClass2 : AbstractClass
+    {
+        public string? Adderss { get; set; }
+        public float Salary { get; set; }
+        public override string Employee()
+        {
+            return (" Name :" + Name + "\n Id :" + Id + "\n Company Name :" + CompanyName + "\n Phone Number :" + PhoneNumber + "\n Adderss :" + Adderss + "\n Salary :" + Salary);
+        }
+    }
+
+    //Interface
+    interface IStudent
+    {
+        public int Id { get; set; }
+        public abstract string Show();
+
+    }
+    interface IStudent1
+    {
+        public string Name { get; set; }
+        public abstract string Show();
+
+    }
+    public class StudentData
+    {
+        public int rollNo { get; set; }
+    }
+    public class Student : StudentData,IStudent, IStudent1
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+       // public int rollNo { get; set; }
+
+        string IStudent1.Name
+        {
+            set
+            {
+                this.Name = value;
+            }
+            get
+            {
+                return this.Name;
+            }
+        }
+
+
+        int IStudent.Id
+        {
+            set
+            {
+                this.Id = value;
+            }
+            get
+            {
+                return this.Id;
+            }
+        }
+        // class
+        int RollNo
+        {
+            set
+            {
+                this.rollNo = value;
+            }
+            get
+            {
+                return this.rollNo;
+            }
+        }
+        public string Show()
+        {
+            return "aaa";
+        }
+        string IStudent1.Show()
+        {
+            return "bb";
+        }
+    }
+}
